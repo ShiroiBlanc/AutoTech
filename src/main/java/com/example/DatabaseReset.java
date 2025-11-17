622a04e56fdfe2225e46e43e648f0249d472d242
Isaiah <isaiah.gabucan@gmail.com>
2025-11-18 04:02:48 +0800

    update

--- src/main/java/com/example/DatabaseReset.java
+++ src/main/java/com/example/DatabaseReset.java
@@ -18,7 +18,7 @@ public class DatabaseReset {
             // First, connect to MySQL server without specifying database
             String url = "jdbc:mysql://localhost:3306";
             String user = "root";
-            String password = "blueelement1";
+            String password = "root";
             
             try (Connection conn = DriverManager.getConnection(url, user, password);
                  Statement stmt = conn.createStatement()) {
